1e9bf959d314c916b9aa719e5fe1abf97db49a25
Olivia Kyle <oliviakatekyle@gmail.com>
2022-02-14 18:07:53 +1300

    initial installs

---
+++ ubuntu.sh
@@ -0,0 +1,21 @@
+# apt update
+sudo apt update
+
+# git
+sudo apt-get install -y git
+
+# steam
+sudo apt-get install -y steam
+
+# vscode 
+sudo snap install vscode
+
+# spotify
+sudo snap install spotify
+
+# Brave Browser
+sudo apt install -y apt-transport-https curl
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install -y brave-browser
